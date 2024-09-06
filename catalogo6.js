36e0fdb0d095aafbfa6a9e20a7aee16a3d9342c6
Hulala <yang.ye.1@hotmail.com>
2024-09-06 11:01:46 +0200

    Update data

--- catalogo6.js
+++ catalogo6.js
@@ -1,28 +1,23 @@
 'strict use'
 const { app } = require("indesign");
 const datas = require("./json/data.json")
+const { datas } = require("function.js")
 
 document.getElementById("catalogo6").onclick = createPage
 
 async function createPage() {
-
-    //Document setting
     app.documentPreferences.pageHeight = '225mm'
     app.documentPreferences.pageWidth = '190mm'
     app.documentPreferences.pagesPerDocument = 1
     app.documentPreferences.startPageNumber = 1
     app.documentPreferences.facingPages = true
     const myDocument = app.documents.add();
-
-    //First page setting for not use
     const myTextFrame = myDocument.pages.item(0).textFrames.add();
     myTextFrame.geometricBounds = ["20mm", "20mm", "40mm", "140mm"];
     myTextFrame.contents = "FIRST PAGE, DO NOT USE IT FOR NOTHING";
-    
     for (const data of datas) {
         const page = myDocument.pages.add()
-    
     }
-
 }
+
 
