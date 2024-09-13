41d7323dfa2c07ab0412d1f7f14896e03f95a0b9
Hulala <yang.ye.1@hotmail.com>
2024-09-13 17:54:15 +0200

    Plantilla catalogo

--- catalogo6.js
+++ catalogo6.js
@@ -4,13 +4,11 @@ const fUXP = require("./function.js")
 const parameter = { pageHeight: 225, pageWidth: 190, startHeight: 30, startWidth: 20, endHeight: 225 - 20, endWidth: 190 - 20, }
 console.clear()
 createDoc = () => {
-    try {
-        const Doc = fUXP.setConfig(app, parameter)
-        for (const data of datas) {
-            const page = createPage(Doc, data)
-        }
-    } catch (error) {
-        console.log(error)
+    const Doc = fUXP.setConfig(app, parameter)
+    console.log(Doc.pages.lastItem())
+    fUXP.addText(Doc.pages.lastItem(), { content: "FIRST PAGE, DO NOT USE IT FOR NOTHING", size_x: 100, size_y: 100 })
+    for (const data of datas) {
+        const page = createPage(Doc, data)
     }
 }
 
@@ -19,18 +17,18 @@ createPage = (Doc, data) => {
     const page = Doc.pages.add()
     if (page.index === 0) {
         fUXP.addImagen(page, { content: "C:\\Proyecto/InDesign-RivaCold-BC_UXP/assets/catalogo6/bg/border1.png" })
-        fUXP.addText(page, { content: data.Label, y: 15, size_x: 18, size_y: 18, justification: Justification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 7.5, Style: 2 })
-        fUXP.addText(page, { content: data.Label, x: 5, y: 105, size_x: 70, size_y: 8, rotation: 90, justification: Justification.RIGHT_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 11, Style: 3 })
-        fUXP.addText(page, { content: page.properties.name, y: parameter.pageHeight, geo_y: 1, size_x: 18, size_y: 10, justification: Justification.CENTER_ALIGN, Font: 0, fontSize: 9, Style: 1 })
-        fUXP.addText(page, { content: "PRECIOS VÁLIDOS EN LA PENÍNSULA DESDE ABRIL DE 2025", x: 22, y: parameter.pageHeight, geo_y: 1, size_x: 150, size_y: 10, Font: 0, fontSize: 7, Style: 1 })
-        fUXP.addText(page, { content: "www.e-bcsystems.com", x: 5, y: 210, size_x: 110, size_y: 10, rotation: 90, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 21, Style: 1 })
+        fUXP.addText(page, { content: data.Label, y: 15, size_x: 18, size_y: 18, justification: Justification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 7.5, Style: 2, tfillColor: Doc.swatches.itemByName("r255g255b255"), bgfillColor: Doc.swatches.itemByName("rivacold nou") })
+        fUXP.addText(page, { content: data.Label, x: 5, y: 105, size_x: 70, size_y: 8, rotation: 90, justification: Justification.RIGHT_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 11, Style: 3, tfillColor: Doc.swatches.itemByName("c0m0y0k68") })
+        fUXP.addText(page, { content: page.properties.name, y: parameter.pageHeight, geo_y: 1, size_x: 18, size_y: 10, justification: Justification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 9, Style: 1, tfillColor: Doc.swatches.itemByName("r255g255b255"), bgfillColor: Doc.swatches.itemByName("C=92 M=62 Y=27 K=58") })
+        fUXP.addText(page, { content: "PRECIOS VÁLIDOS EN LA PENÍNSULA DESDE ABRIL DE 2025", x: 22, y: parameter.pageHeight, geo_y: 1, size_x: 150, size_y: 10, fontSize: 7, Style: 1, tfillColor: Doc.swatches.itemByName("c0m0y0k68") })
+        fUXP.addText(page, { content: "www.e-bcsystems.com", x: 5, y: 210, size_x: 110, size_y: 10, rotation: 90, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 21, Style: 1, tfillColor: Doc.swatches.itemByName("rivacold nou"), tfillTint: 30 })
     } else {
         fUXP.addImagen(page, { content: "C:\\Proyecto/InDesign-RivaCold-BC_UXP/assets/catalogo6/bg/border1.png", x: parameter.pageWidth * 2, geo_x: 1 })
-        fUXP.addText(page, { content: data.Label, x: parameter.pageWidth * 2, y: 15, geo_x: 1, size_x: 18, size_y: 18, justification: Justification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 7.5, Style: 2 })
-        fUXP.addText(page, { content: data.Label, x: parameter.pageWidth * 2 - 5, y: 105, geo_x: 1, size_x: 70, size_y: 8, rotation: 90, justification: Justification.RIGHT_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 11, Style: 3 })
-        fUXP.addText(page, { content: page.properties.name, x: parameter.pageWidth * 2, y: parameter.pageHeight, geo_y: 1, geo_x: 1, size_x: 18, size_y: 10, justification: Justification.CENTER_ALIGN, Font: 0, fontSize: 9, Style: 1 })
-        fUXP.addText(page, { content: "PRECIOS VÁLIDOS EN LA PENÍNSULA DESDE ABRIL DE 2025", x: parameter.pageWidth * 2 - 22, y: parameter.pageHeight, geo_y: 1, geo_x: 1, size_x: 150, size_y: 10, justification: Justification.RIGHT_ALIGN, Font: 0, fontSize: 7, Style: 1 })
-        fUXP.addText(page, { content: "www.e-bcsystems.com", x: parameter.pageWidth * 2 - 5, y: 210, geo_x: 1, size_x: 110, size_y: 10, rotation: 90, vertical: VerticalJustification.CENTER_ALIGN, Font: 0, fontSize: 21, Style: 1 })
+        fUXP.addText(page, { content: data.Label, x: parameter.pageWidth * 2, y: 15, geo_x: 1, size_x: 18, size_y: 18, justification: Justification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 7.5, Style: 2, tfillColor: Doc.swatches.itemByName("r255g255b255"), bgfillColor: Doc.swatches.itemByName("rivacold nou") })
+        fUXP.addText(page, { content: data.Label, x: parameter.pageWidth * 2 - 5, y: 105, geo_x: 1, size_x: 70, size_y: 8, rotation: 90, justification: Justification.RIGHT_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 11, Style: 3, tfillColor: Doc.swatches.itemByName("c0m0y0k68") })
+        fUXP.addText(page, { content: page.properties.name, x: parameter.pageWidth * 2, y: parameter.pageHeight, geo_y: 1, geo_x: 1, size_x: 18, size_y: 10, justification: Justification.CENTER_ALIGN, fontSize: 9, Style: 1, tfillColor: Doc.swatches.itemByName("r255g255b255"), bgfillColor: Doc.swatches.itemByName("C=92 M=62 Y=27 K=58"), vertical: VerticalJustification.CENTER_ALIGN, })
+        fUXP.addText(page, { content: "PRECIOS VÁLIDOS EN LA PENÍNSULA DESDE ABRIL DE 2025", x: parameter.pageWidth * 2 - 22, y: parameter.pageHeight, geo_y: 1, geo_x: 1, size_x: 150, size_y: 10, justification: Justification.RIGHT_ALIGN, fontSize: 7, Style: 1, tfillColor: Doc.swatches.itemByName("c0m0y0k68") })
+        fUXP.addText(page, { content: "www.e-bcsystems.com", x: parameter.pageWidth * 2 - 5, y: 210, geo_x: 1, size_x: 110, size_y: 10, rotation: 90, vertical: VerticalJustification.CENTER_ALIGN, fontSize: 21, Style: 1, tfillColor: Doc.swatches.itemByName("rivacold nou"), fillTint: 30 })
     }
 
     return page
--- function.js
+++ function.js
@@ -1,41 +1,47 @@
-const { FitOptions } = require("indesign");
+const { FitOptions, SaveOptions } = require("indesign");
 const Font = ["Frutiger LT Std"]
 const Style = ["45 Light", "55 Roman", "65 Bold", "75 Black", "95 Ultra Black"]
-
+const dir = "C:\\Proyecto\\InDesign-RivaCold-BC_UXP"
+let Doc
 module.exports.setConfig = (app, parameter) => {
+    //app.documents.everyItem().close(SaveOptions.NO)
+    app.documents.firstItem().close(SaveOptions.NO)
+    app.updateFonts()
+    app.loadSwatches(dir + "/data/indd/Proves components Yang.indd")
     app.documentPreferences.pageHeight = parameter.pageHeight
     app.documentPreferences.pageWidth = parameter.pageWidth
     app.documentPreferences.pagesPerDocument = 1
     app.documentPreferences.startPageNumber = 1
-    app.documentPreferences.facingPages = true
-    const myDocument = app.documents.add();
-    const myTextFrame = myDocument.pages.item(0).textFrames.add();
-    myTextFrame.geometricBounds = [parameter.startHeight, parameter.startWidth, parameter.endHeight, parameter.endWidth];
-    myTextFrame.contents = "FIRST PAGE, DO NOT USE IT FOR NOTHING";
-    return myDocument
+    return Doc = app.documents.add();
 }
 module.exports.addImagen = (page, data) => {
     const frame = page.textFrames.add()
     doPlace(frame, data)
     doMove(frame, data)
+    return frame
 }
 
 module.exports.addText = (page, data) => {
-    console.log(Style[data.Style])
+    console.log(page)
     const frame = page.textFrames.add({
         contents: data.content,
         geometricBounds: [0, 0, data.size_y, data.size_x],
         strokeWidth: 0,
         parentStory: {
-            appliedFont: Font[data.Font],
-            fontStyle: Style[data.Style],
+            appliedFont: Font[data.Font || 0],
+            fontStyle: Style[data.Style || 0],
             pointSize: data.fontSize,
-            justification: data.justification
+            justification: data.justification,
+            fillColor: data.tfillColor || Doc.swatches.itemByName("r0g0b0 44"),
+            fillTint: data.tfillTint || 100,
         },
+        fillColor: data.bgfillColor || Doc.swatches.itemByName("r255g255b255"),
+        fillTint: data.bgfillTint || 100,
     })
     if (data.vertical) frame.textFramePreferences.verticalJustification = data.vertical
     if (data.rotation) frame.absoluteRotationAngle = data.rotation
     doMove(frame, data)
+    return frame
 }
 
 module.exports.addTable = () => { }
@@ -45,5 +51,5 @@ doPlace = (frame, data) => {
     frame.fit(FitOptions.FRAME_TO_CONTENT)
 }
 doMove = (frame, data) => {
-    frame.move([(data.x | 0) - (frame.geometricBounds[3] - frame.geometricBounds[1]) * (data.geo_x | 0), (data.y | 0) - (frame.geometricBounds[2] - frame.geometricBounds[0]) * (data.geo_y | 0),])
+    frame.move([(data.x || 0) - (frame.geometricBounds[3] - frame.geometricBounds[1]) * (data.geo_x || 0), (data.y || 0) - (frame.geometricBounds[2] - frame.geometricBounds[0]) * (data.geo_y || 0),])
 }
